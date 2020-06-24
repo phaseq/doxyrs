234bccfd41aebf70a63c94f1f4ffc520af505894
Fabian Beckmann <fabianb@moduleworks.com>
2020-06-24 12:16:36 +0200

    implement enum value descriptions, links

--- src/parser.rs
+++ src/parser.rs
@@ -46,6 +46,15 @@ pub struct Member {
     pub ref_id: String,
     pub definition: String,
     pub description: String,
+    pub enum_values: Vec<EnumValue>,
+}
+
+#[derive(Serialize)]
+pub struct EnumValue {
+    pub ref_id: String,
+    pub name: String,
+    pub initializer: Option<String>,
+    pub description: String,
 }
 
 struct Context {
@@ -246,25 +255,20 @@ fn parse_member(memberdef: Node, mut context: &mut Context) -> Member {
             "{}<span class=\"keyword\">using</span> <span class=\"member_name\">{}</span> = <span class=\"type\">{}</span>",
             template, name, return_type
         ),
-        "variable" | "property" => format!(
-            "<span class=\"type\">{}</span> <span class=\"member_name\">{}</span> <span class=\"defval\">{}</span>",
-            return_type,
-            name,
-            tera::escape_html(memberdef.get_child_value("initializer").unwrap_or_default())
-        ),
+        "variable" | "property" => {
+            let defval = if let Some(initializer) = memberdef.get_child("initializer"){
+                format!(" <span class=\"defval\">{}</span>", parse_text(initializer, &mut context))
+            } else {
+                "".to_owned()
+            };
+            format!(
+                "<span class=\"type\">{}</span> <span class=\"member_name\">{}</span>{}",
+                return_type,
+                name,
+                defval)
+        },
         "enum" => {
-            // TODO: embed documentation of enum parameters
-            let mut s = String::new();
-            s.push_str(&format!("<span class=\"keyword\">enum</span> <span class=\"member_name\">{}</span> {{<br>", name));
-            for value in memberdef.children().filter(|c| c.has_tag_name("enumvalue")) {
-                s.push_str(&format!(
-                    "&nbsp;&nbsp;&nbsp;&nbsp;<span class=\"declname\">{}</span> <span class=\"defval\">{}</span><br/>",
-                    value.get_child_value("name").unwrap(),
-                    value.get_child_value("initializer").unwrap_or_default()
-                ))
-            }
-            s.push_str("}");
-            s
+            format!("<span class=\"keyword\">enum</span> <span class=\"member_name\">{}</span>", name)
         },
         //_ => format!("{}", memberdef.get_child_value("definition").unwrap()),
         _ => panic!(
@@ -272,6 +276,34 @@ fn parse_member(memberdef: Node, mut context: &mut Context) -> Member {
             memberdef.attribute("kind").unwrap(),
             ref_id
         ),
+    };
+
+    let enum_values = if memberdef.attribute("kind").unwrap() == "enum" {
+        memberdef
+            .children()
+            .filter(|c| c.has_tag_name("enumvalue"))
+            .map(|value| {
+                let description = {
+                    let brief =
+                        parse_text(value.get_child("briefdescription").unwrap(), &mut context);
+                    let detailed = parse_text(
+                        value.get_child("detaileddescription").unwrap(),
+                        &mut context,
+                    );
+                    brief + &detailed
+                };
+                EnumValue {
+                    ref_id: value.attribute("id").unwrap().to_owned(),
+                    name: value.get_child_value("name").unwrap().to_owned(),
+                    initializer: value
+                        .get_child("initializer")
+                        .map(|i| parse_text(i, &mut context)),
+                    description,
+                }
+            })
+            .collect()
+    } else {
+        vec![]
     };
 
     let description = {
@@ -289,6 +321,7 @@ fn parse_member(memberdef: Node, mut context: &mut Context) -> Member {
         ref_id,
         definition,
         description,
+        enum_values,
     }
 }
 
